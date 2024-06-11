8a9873b356db7f47b65518f7baca6666f6624608
Breece1019 <breece1019@gmail.com>
2024-06-10 22:25:56 -0400

    idea

--- src/Model.java
+++ src/Model.java
@@ -9,6 +9,7 @@ import org.jsoup.select.Elements;
 public class Model {
     private Document doc;
     private Map<String, Region> RegionList = new HashMap<>();
+    private int[] indexMemory; // some way to store the collapse# when a region is removed
 
     Model(Document doc) {
         this.doc = doc;
@@ -38,6 +39,7 @@ public class Model {
 
     void addRegion(String regionName) {
         // I know this looks super gross but it works, I will change it later
+        // TODO fix href="#collapse1" to be the correct number
         String html = "<div class=\"panel panel-default\"><div class=\"panel-heading\"><h4 class=\"panel-title\"><a class=\"accordion-toggle\" data-toggle=\"collapse\" data-parent=\"#accordion\" href=\"#collapse1\">" + regionName + "</a></h4></div><div id=\"collapse1\" class=\"panel-collapse collapse\"><div class=\"panel-body\"><dl></dl></div></div></div>";
         Element newRegion = Jsoup.parseBodyFragment(html, "UTF-8").selectFirst("div[class=\"panel panel-default\"]");
         this.doc.selectFirst("div[id=\"accordion\"]").appendChild(newRegion);
